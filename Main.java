329b3a23877cce1955d10c93a7648e83e9ea6727
Kitt0ny <dima0908074@mail.ru>
2025-05-24 13:25:49 +0400

    edit status repository

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Я elfktyysq репозиторий.");
+        System.out.println("Я удаленный репозиторий.");
         System.out.println("24.05.2025");
     }
 }
